543758bbfe247e87127acbb85c2c9ae1feb9ccac
Mertcan Keskin <60985066+keskinmertcan@users.noreply.github.com>
2020-02-25 23:22:17 +0300

    Upload Website

---
+++ index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css" type="text/css">
+<title>Yazilimcim.net</title>
+</head>
+
+<body>
+	<div class="anabaslik">
+		<div class="wrap">
+			<div class="baslik">
+				<div class="menuwrap">
+					<ul class="majestic">
+						<li>
+							<a href="index.php" id="home">
+								<img src="images/header/home.png" width="20px"><br>
+								<strong>ANASAYFA</strong><br>
+								<small>Yazılım İndir</small>
+							</a>
+						</li>
+						<li>
+							<a href="blog.php" id="blog">
+								<img src="images/header/pen.png" width="20px"><br>
+								<strong>BLOG</strong><br>
+								<small>Güncel Yazılar</small>
+							</a>
+						</li>
+						<li>
+							<a href="araclar.php" id="araclar">
+								<img src="images/header/wreck.png" width="20px"><br>
+								<strong>ARAÇLAR</strong><br>
+								<small>Yardımcı Araçlar</small>
+							</a>
+						</li>
+						<li>
+							<a href="iletisim.php" id="iletisim">
+								<img src="images/header/mail.png" width="20px"><br>
+								<strong>İLETİŞİM</strong><br>
+								<small>Bize Ulaşın!</small>
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div class="logo">
+					<a href="index.php" alt="Yazilimcim.net"><img src="images/logo/yazilimcim.png"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="kucukbaslik" id="navcount">
+		<div class="wrap">
+			<div class="sagblok">
+				<div class="kullanici">
+					<ul class="kullanicimenu">
+						<li>
+							<a href="login.php">Giriş Yap</a>
+						</li>
+						<li>
+							<a href="signup.php">Üye Ol!</a>
+						</li>
+						<li>
+							<form class="searchbox" role="search" method="get" action="index.php"> 
+								<input required="" type="search" placeholder="Sitede ara..." name="s" class="searchbox-input" onkeyup="buttonUp();"> 
+								<input type="submit" class="searchbox-submit" value="Ara"> 
+							</form>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="solblok">
+				<div class="sosyalmenu">
+					<ul>
+						<li>
+							<a href="https://www.facebook.com" title="Facebook sayfamızı beğenin."><img src="images/social/facebook.png" width="42px" height="42px"></a>
+						</li>
+						<li>
+							<a href="https://www.instagram.com" title="Instagram'dan bizi takip edin."><img src="images/social/instagram.png" width="42px" height="42px"></a>
+						</li>
+						<li>
+							<a href="https://www.youtube.com" title="Youtube kanalımıza abone olun."><img src="images/social/youtube.png" width="42px" height="42px"></a>
+						</li>
+						<li>
+							<a href="https://www.whatsapp.com" title="Bize Whatsapp'tan ulaşın."><img src="images/social/whatsapp.png" width="42px" height="42px"></a>
+						</li>
+						<li>
+							<a href="https://www.twitter.com" title="Twitter'dan takip edin."><img src="images/social/twitter.png" width="42px" height="42px"></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="clear"></div>
+	<div id="wrap">
+		<div style="width: 100%; text-align: center; position: relative; font-size: 16px; font-weight: bold; padding: 10px 0; margin-bottom: 10px; color: #E74C3C; background: #000000; border: 1px solid #E74C3C; clear: both;">
+			<span style="width: 100%; display: inline-block;">>>>> SİTEMİZDE UYGULAMALAR HAFTASONLARI GÜNCELLENMEKTEDİR.</span>
+		</div>
+		<div id="content">
+			<style>
+				#slider-code{height:218px;background:#1b1b1b;-moz-border-radius:4px;-webkit-border-radius:4px;border-radius:4px;border:1px solid #434343;float:left;owerflow:hidden;margin:0 0 10px;padding:5px 2px 5px 5px;}#slider-code .viewport{float:left;width:972px;height:218px;overflow:hidden;position:relative;}#slider-code .buttons{display:block;float:left;margin:0px 6px 0 0;}#slider-code .next{margin:0px 0 0 6px;}#slider-code .disable{visibility:hidden;}#slider-code .overview{list-style:none;position:absolute;left:0;top:0;margin:0;padding:0;}#slider-code .overview li{float:left;height:205px;width:147px;background:#353535;-moz-border-radius:4px;-webkit-border-radius:4px;border-radius:4px;border:1px solid #434343;margin:0 3px 0 0;padding:5px;}#slider-code .overview li:hover{background:rgba(150,150,150,0.5);transition:all .3s linear;-o-transition:all .3s linear;-moz-transition:all .3s linear;-webkit-transition:all .3s linear;}#slider-code .buttons img,#slider-code .next img,#slider-code .overview li img{border:0;}
+				img.guncover{float:right;position:relative;z-index:1;margin-left:-30px;-moz-border-radius:4px;-webkit-border-radius:4px;border-radius:4px;border:0;}
+			</style>
+			<div id="slider-code">
+				<div class="viewport">
+					<ul class="overview">
+						<li>
+							<a href="pages/photoshop2020.php">
+								<img src="images/uploads/photoshop.png" alt="Photoshop CC 2020 İndir" width="147px" height="207px">
+							</a>
+						</li>
+						<li>
+							<a href="pages/dreamweaver.php">
+								<img src="images/uploads/dreamweaver.png" alt="Dreamweaver CC 2020 İndir" width="147px" height="207px">
+							</a>
+						</li>
+						<li>
+							<a href="pages/windows10.php">
+								<img src="images/uploads/windows10.png" alt="Windows 10 İndir" width="147px" height="207px">
+							</a>
+						</li>
+						<li>
+							<a href="pages/avast.php">
+								<img src="images/uploads/avast.png" alt="Avast İndir" width="147px" height="207px">
+							</a>
+						</li>
+						<li>
+							<a href="pages/kaspersky.php">
+								<img src="images/uploads/kaspersky.png" alt="Kaspersky İndir" width="147px" height="207px">
+							</a>
+						</li>
+						<li>
+							<a href="pages/office2019.php">
+								<img src="images/uploads/office2019.png" alt="Office 2019 İndir" width="147px" height="207px">
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
